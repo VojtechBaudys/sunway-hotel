c9ec2c21038c15a19bf9b7ccefefedca46791ccb
Vojtech Baudys <vojta@baudys.name>
2024-12-23 00:05:29 +0100

    button component, fixes

--- Client/src/pages/hotels/index.tsx
+++ Client/src/pages/hotels/index.tsx
@@ -9,7 +9,7 @@ function Hotels(): React.ReactElement {
 	const hotels: IHotel[] = data;
 
 	return (
-		<section className='py-8 antialiased md:py-12 w-full'>
+		<section className='py-8 md:py-12 w-full'>
 			{!loading
 				? (
 					<div className='mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4'>
