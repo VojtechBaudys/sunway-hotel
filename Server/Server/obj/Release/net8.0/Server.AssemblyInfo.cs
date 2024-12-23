6f628850afebfa0b0a0e07d6e9495fed26c70a7c
Vojtech Baudys <vojta@baudys.name>
2024-12-23 06:40:58 +0100

    data hotfix

--- Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Release/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efcf48cba4336eb143ffc90a38b0de0be802e1a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76188e33db04c9606b9a10020f2e5718fa57a88a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
